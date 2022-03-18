b6c0fd84c88ba32575e34ab0a76116d05d90b954
Daniel Boullon <daniel.boullon@hotmail.es>
2022-03-18 08:38:43 -0300

    Se agrego checkeo de mysql al /health

--- src/main/java/ar/com/dboullon/login/controller/HealthController.java
+++ src/main/java/ar/com/dboullon/login/controller/HealthController.java
@@ -1,5 +1,9 @@
 package ar.com.dboullon.login.controller;
 
+import ar.com.dboullon.login.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
-    @GetMapping
-    public ResponseEntity<?> health(){
-        return  ResponseEntity.ok("OK");
+    @Autowired
+    TestRepository testRepository;
+
+    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> health() {
+        try{
+            String version = testRepository.test();
+            return ResponseEntity
+                    .ok("{\"status\": \"Healthy\"," +
+                    "\"mysql_version\":\"" + version + "\"}");
+
+        }  catch (Exception e){
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"status\": \"Unhealthy\"," +
+                    "\"description\":\"" + e.getMessage() + "\"}");
+        }
+
     }
 }
